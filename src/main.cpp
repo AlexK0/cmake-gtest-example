959f14c00fd40aad9a572f400ed6acbfaa0c3498
Aleksandr Korepanov <korepanov.sasha@gmail.com>
2021-03-07 19:31:05 +0300

    Bidirect iterator.

--- src/main.cpp
+++ src/main.cpp
@@ -3,93 +3,94 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
+#include <algorithm>
 
 #include "sequence-produce-iterator.h"
 #include "shuffle-access-iterator.h"
 
-void case1() {
-  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-  std::vector<int> target(20, 0);
-
-  for (auto i: target) {
-    std::cout << i << ", ";
-  }
-  std::cout << std::endl;
-
-  ShuffleAccessIterator it{target};
-  std::copy(v.begin(), v.end(), it);
-
-
-  for (auto i: target) {
-    std::cout << i << ", ";
-  }
-  std::cout << std::endl;
-}
-
-void case2() {
-  // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-  std::vector<int> target(20, 0);
-
-  for (auto i: target) {
-    std::cout << i << ", ";
-  }
-  std::cout << std::endl;
-
-  ShuffleAccessIterator it{target};
-  for (int i = 0; i < 20; ++i) {
-    *it = i;
-  }
-
-  for (auto i: target) {
-    std::cout << i << ", ";
-  }
-  std::cout << std::endl;
-}
-
-void case3() {
-  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
-}
-
-void case4() {
-  std::vector<int> v{SequenceProduceIterator<int>{1, 10, 10}, SequenceProduceIterator<int>{}};
-
-  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
-}
-
-void case5() {
-  std::copy(SequenceProduceIterator<double>{0.2, 0.3, 10},
-            SequenceProduceIterator<double>{},
-            std::ostream_iterator<double>(std::cout, " "));
-  std::cout << std::endl;
-}
-
-void case6() {
-  std::vector<std::string> target(5, "");
-
-  std::copy(SequenceProduceIterator<std::string>{"a", "+x", 10},
-            SequenceProduceIterator<std::string>{},
-            ShuffleAccessIterator{target});
-
-  std::copy(target.begin(), target.end(), std::ostream_iterator<std::string>(std::cout, " "));
-  std::cout << std::endl;
-}
-
-void case7() {
-  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-  std::copy(ShuffleAccessIterator<int>(v), ShuffleAccessIterator<int>(v, 0),
-            std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
-
-  std::copy(ShuffleAccessIterator<int>(v, 20), ShuffleAccessIterator<int>(v, 0),
-            std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
-}
+//void case1() {
+//  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//  // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//  std::vector<int> target(20, 0);
+//
+//  for (auto i: target) {
+//    std::cout << i << ", ";
+//  }
+//  std::cout << std::endl;
+//
+//  ShuffleAccessIterator it{target};
+//  std::copy(v.begin(), v.end(), it);
+//
+//
+//  for (auto i: target) {
+//    std::cout << i << ", ";
+//  }
+//  std::cout << std::endl;
+//}
+//
+//void case2() {
+//  // 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+//  std::vector<int> target(20, 0);
+//
+//  for (auto i: target) {
+//    std::cout << i << ", ";
+//  }
+//  std::cout << std::endl;
+//
+//  ShuffleAccessIterator it{target};
+//  for (int i = 0; i < 20; ++i) {
+//    *it = i;
+//  }
+//
+//  for (auto i: target) {
+//    std::cout << i << ", ";
+//  }
+//  std::cout << std::endl;
+//}
+//
+//void case3() {
+//  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+//  std::cout << std::endl;
+//}
+//
+//void case4() {
+//  std::vector<int> v{SequenceProduceIterator<int>{1, 10, 10}, SequenceProduceIterator<int>{}};
+//
+//  std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+//  std::cout << std::endl;
+//}
+//
+//void case5() {
+//  std::copy(SequenceProduceIterator<double>{0.2, 0.3, 10},
+//            SequenceProduceIterator<double>{},
+//            std::ostream_iterator<double>(std::cout, " "));
+//  std::cout << std::endl;
+//}
+//
+//void case6() {
+//  std::vector<std::string> target(5, "");
+//
+//  std::copy(SequenceProduceIterator<std::string>{"a", "+x", 10},
+//            SequenceProduceIterator<std::string>{},
+//            ShuffleAccessIterator{target});
+//
+//  std::copy(target.begin(), target.end(), std::ostream_iterator<std::string>(std::cout, " "));
+//  std::cout << std::endl;
+//}
+//
+//void case7() {
+//  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//
+//  std::copy(ShuffleAccessIterator<int>(v), ShuffleAccessIterator<int>(v, 0),
+//            std::ostream_iterator<int>(std::cout, " "));
+//  std::cout << std::endl;
+//
+//  std::copy(ShuffleAccessIterator<int>(v, 20), ShuffleAccessIterator<int>(v, 0),
+//            std::ostream_iterator<int>(std::cout, " "));
+//  std::cout << std::endl;
+//}
 
 void case8() {
   std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -171,6 +172,58 @@ void case13() {
   std::cout << std::accumulate(shuffle_range.begin(), shuffle_range.end(), int{0}) << std::endl;
 }
 
+void case14() {
+  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  ShuffleAccessRange shuffle_range{v, 7};
+
+  for (auto it = shuffle_range.begin(); it != shuffle_range.end(); ++it) {
+    std::cout << *it << ", ";
+  }
+  std::cout << std::endl;
+
+  for (auto it = shuffle_range.end(); it != shuffle_range.begin();) {
+    std::cout << *--it << ", ";
+  }
+  std::cout << std::endl;
+}
+
+void case15() {
+  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+  ShuffleAccessRange shuffle_range{v};
+
+  for (auto it = shuffle_range.begin(); it != shuffle_range.end(); ++it) {
+    std::cout << *it << ", ";
+  }
+  std::cout << std::endl;
+
+  for (auto it = shuffle_range.rbegin(); it != shuffle_range.rend(); ++it) {
+    std::cout << *it << ", ";
+  }
+  std::cout << std::endl;
+}
+
+void case16() {
+  std::vector<int> v{SequenceProduceIterator<int>(1, 1, 1000), SequenceProduceIterator<int>()};
+  ShuffleAccessRange shuffle_range{v, 3};
+
+  do {
+    std::copy(shuffle_range.begin(), shuffle_range.end(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << std::endl;
+  } while(std::next_permutation(shuffle_range.begin(), shuffle_range.end()));
+}
+
+
+void case17() {
+  std::vector<int> v{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  ShuffleAccessRange shuffle_range{v};
+
+  auto first = std::next(shuffle_range.begin(), 2);
+  auto last = std::prev(shuffle_range.end(), 3);
+  std::copy(first, last, std::ostream_iterator<int>(std::cout, ", "));
+  std::cout << std::endl;
+}
+
+
 int main(int argc, char *argv[]) {
   int case_id = 0;
   if (argc > 1) {
@@ -178,27 +231,27 @@ int main(int argc, char *argv[]) {
   }
 
   switch (case_id) {
-    case 1:
-      case1();
-      break;
-    case 2:
-      case2();
-      break;
-    case 3:
-      case3();
-      break;
-    case 4:
-      case4();
-      break;
-    case 5:
-      case5();
-      break;
-    case 6:
-      case6();
-      break;
-    case 7:
-      case7();
-      break;
+//    case 1:
+//      case1();
+//      break;
+//    case 2:
+//      case2();
+//      break;
+//    case 3:
+//      case3();
+//      break;
+//    case 4:
+//      case4();
+//      break;
+//    case 5:
+//      case5();
+//      break;
+//    case 6:
+//      case6();
+//      break;
+//    case 7:
+//      case7();
+//      break;
     case 8:
       case8();
       break;
@@ -216,6 +269,18 @@ int main(int argc, char *argv[]) {
       break;
     case 13:
       case13();
+      break;
+    case 14:
+      case14();
+      break;
+    case 15:
+      case15();
+      break;
+    case 16:
+      case16();
+      break;
+    case 17:
+      case17();
       break;
     default:
       std::cout << "Please choose the correct case!" << std::endl;
--- src/shuffle-access-iterator.h
+++ src/shuffle-access-iterator.h
@@ -2,61 +2,93 @@
 
 #include <vector>
 #include <random>
+#include <unordered_map>
 
 template<class T>
 class ShuffleAccessIterator {
 public:
-  ShuffleAccessIterator(std::vector<T> &target, size_t left) noexcept:
-          random_gen_{std::random_device{}()}, distribution_{0, target.size() - 1},
-          target_{target}, left_(left), next_{distribution_(random_gen_)} {}
-
-  explicit ShuffleAccessIterator(std::vector<T> &target) noexcept: ShuffleAccessIterator{target, target.size()} {}
+  ShuffleAccessIterator(std::vector<T> &target, size_t left, size_t total, std::random_device::result_type seed) noexcept:
+          seed_{seed},
+          random_gen_{seed_},
+          distribution_{0, target.size() - 1},
+          target_{&target},
+          left_iterations_{left},
+          total_iterations_{total} {
+    random_gen_.discard(total_iterations_ - left_iterations_);
+    if (left_iterations_) {
+      next_ = distribution_(random_gen_);
+    }
+  }
 
   using value_type = T;
   using difference_type = std::ptrdiff_t;
   using pointer = T *;
   using reference = T &;
-  using iterator_category = std::forward_iterator_tag;
+  using iterator_category = std::bidirectional_iterator_tag;
 
-  reference operator*() noexcept { return target_[next_]; }
+  reference operator*() noexcept { return (*target_)[next_]; }
+  pointer operator->() noexcept { return &((*target_)[next_]); }
 
-  pointer operator->() noexcept { return &target_[next_]; }
-
-  bool operator!=(const ShuffleAccessIterator &other) const {
-    if (left_ == other.left_ && left_ == 0) {
-      return &target_ != &other.target_;
-    }
-    return left_ != other.left_ || random_gen_ != other.random_gen_ || &target_ != &other.target_;
+  bool operator==(const ShuffleAccessIterator &other) const {
+    return left_iterations_ == other.left_iterations_ && total_iterations_ == other.total_iterations_ &&
+            seed_ == other.seed_ && target_ == other.target_;
   }
 
-  // pre-increment: ++it
+  bool operator!=(const ShuffleAccessIterator &other) const {
+    return !(*this == other);
+  }
+
   ShuffleAccessIterator &operator++() noexcept {
-    next_ = distribution_(random_gen_);
-    --left_;
+    if (left_iterations_) {
+      next_ = distribution_(random_gen_);
+      --left_iterations_;
+    } else {
+      // undefined behaviour
+    }
     return *this;
   }
 
-  // post-increment: it++
+  ShuffleAccessIterator &operator--() noexcept {
+  if (left_iterations_ != total_iterations_) {
+      random_gen_.seed(seed_);
+      random_gen_.discard(total_iterations_ - left_iterations_ - 1);
+      next_ = distribution_(random_gen_);
+      ++left_iterations_;
+    } else {
+      // undefined behaviour
+    }
+    return *this;
+  }
+
   ShuffleAccessIterator operator++(int) noexcept {
     ShuffleAccessIterator old = *this;
     ++(*this);
     return old;
   }
 
+  ShuffleAccessIterator operator--(int) noexcept {
+    ShuffleAccessIterator old = *this;
+    --(*this);
+    return old;
+  }
+
 private:
+  std::random_device::result_type seed_{0};
   std::mt19937_64 random_gen_;
   std::uniform_int_distribution<size_t> distribution_;
-  std::vector<T> &target_;
+  std::vector<T> *target_;
   size_t next_{0};
-  size_t left_{0};
+  size_t left_iterations_{0};
+  size_t total_iterations_{0};
 };
 
 template<class T>
 class ShuffleAccessRange {
 public:
   ShuffleAccessRange(std::vector<T> &target, size_t elements) :
-          first_{target, elements},
-          last_{target, 0} {}
+          seed_{std::random_device{}()},
+          first_{target, elements, elements, seed_},
+          last_{target, 0, elements, seed_} {}
 
   explicit ShuffleAccessRange(std::vector<T> &target) :
           ShuffleAccessRange{target, target.size()} {}
@@ -64,7 +96,12 @@ public:
   auto begin() const { return first_; }
   auto end() const { return last_; }
 
+  auto rbegin() const { return std::make_reverse_iterator(last_); }
+  auto rend() const { return std::make_reverse_iterator(first_); }
+
 private:
+  std::random_device::result_type seed_{0};
   ShuffleAccessIterator<T> first_;
   ShuffleAccessIterator<T> last_;
 };
+
